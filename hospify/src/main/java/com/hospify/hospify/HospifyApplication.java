97ca3eb7628b34fd40de91a1d2346b1b392bc304
Rajnish-J <rajnishofficial02@gmail.com>
2024-12-18 13:50:24 +0530

    FEAT: Spring Project started

---
+++ hospify/src/main/java/com/hospify/hospify/HospifyApplication.java
@@ -0,0 +1,14 @@
+package com.hospify.hospify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospifyApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ hospify/src/test/java/com/hospify/hospify/HospifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospify.hospify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
